cfae50b8279b0e45cb8247d56b4e89ade22ed76c
TranDKhoi <tranxyz88@gmail.com>
2023-04-19 08:26:17 +0700

    config app file

---
+++ lib/app/core/values/strings.dart
@@ -0,0 +1,1 @@
+const api_url = "https://cliver-api.azurewebsites.net/api";
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,74 @@
+import 'dart:developer';
+import 'package:cliver_mobile/app/core/utils/utils.dart';
+import 'package:cliver_mobile/data/models/user.dart';
+import 'package:cliver_mobile/data/models/wallet.dart';
+import 'package:cliver_mobile/data/services/AuthService.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'app/app.dart';
+import 'app/controller/user_controller.dart';
+import 'app/core/utils/download_class.dart';
+import 'app/core/values/app_colors.dart';
+import 'app/features/setting/setting_controller.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(debug: true, ignoreSsl: true);
+  FlutterDownloader.registerCallback(DownloadClass.callback);
+  await initData();
+  configLoadingBar();
+  // runApp(const MyApp());
+
+  runApp(
+    DevicePreview(
+      enabled: false,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+initData() async {
+  await Firebase.initializeApp();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  var pref = await SharedPreferences.getInstance();
+  final userController = Get.put(UserController());
+  final settingController = Get.put(SettingController());
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  userController.isFirstTime = pref.getBool("isFirstTime") ?? true;
+  userController.userToken = pref.getString("user_token") ?? "";
+  settingController.isDarkMode.value = pref.getBool("isDark") ?? false;
+  settingController.isVN.value = pref.getBool("isVN") ?? false;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  if (settingController.isDarkMode.value) {
+    AppColors().changeColor(true);
+    Get.changeTheme(AppColors().darkTheme);
+  } else {
+    AppColors().changeColor(false);
+    Get.changeTheme(AppColors().lightTheme);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  if (userController.userToken != "") {
+    var res =
+        await AuthService.instance.getUserData(token: userController.userToken);
+
+    if (res.statusCode == 200) {
+      userController.currentUser.value = User.fromJson(res.body['data']);
+      log("get user infomation success");
+      log("current user: ${userController.currentUser.value.name}");
+    } else {
+      log("get user infomation failed");
+    }
+    res = await AuthService.instance.getWallet(token: userController.userToken);
+    if (res.isOk) {
+      Wallet wallet = Wallet.fromJson(res.body["data"]["wallet"]);
+      userController.currentUser.value.wallet = wallet;
+      log("get user wallet success");
+    } else {
+      log("get user wallet failed");
+    }
   }
 }
