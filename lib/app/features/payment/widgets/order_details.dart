6c7a286f123307449f96a940ab6506ccc5a3d2c7
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-05-16 22:45:55 +0700

    refactor payment screen to small widget

---
+++ lib/app/features/payment/widgets/order_details.dart
@@ -0,0 +1,120 @@
+import 'dart:math' as math;
+import 'package:cliver_mobile/app/core/utils/utils.dart';
+import 'package:cliver_mobile/app/features/payment/payment_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../core/values/app_colors.dart';
+
+class OrderDetails extends StatelessWidget {
+  OrderDetails({super.key});
+  final paymentController = Get.find<PaymentController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const Text(
+                "Order details",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Spacer(),
+              GestureDetector(
+                onTap: () => paymentController.showOrderDetails(),
+                child: Obx(
+                  () => Transform.rotate(
+                    angle: (paymentController.isOrderDetailsClosed.value)
+                        ? -math.pi / 180.0 * -90
+                        : -math.pi / 180.0 * 90,
+                    child: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.black,
+                      size: 25,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Obx(
+            () => Visibility(
+              visible: !paymentController.isOrderDetailsClosed.value,
+              child: SizedBox(
+                height: context.height * 0.07,
+                child: Row(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Icon(
+                        Icons.check,
+                        color: AppColors.primaryColor,
+                        size: 25,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            paymentController.package?.name as String,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const Text(
+                            "Revisions",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          FormatHelper()
+                              .moneyFormat(paymentController.package?.price)
+                              .toString(),
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          "${paymentController.package?.numberOfRevisions}",
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
