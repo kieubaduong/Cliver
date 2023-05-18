e8efaedb65165c7ea5f29106c7893c884548a07c
Le Hai Phong <phong46037@gmail.com>
2023-05-19 02:47:01 +0700

    delete file garbage

--- lib/app/features/buyer/save_list/widgets/widgets.dart
+++
@@ -1,3 +0,0 @@
-export 'rename_save_list.dart';
-export 'service_save_list.dart';
-export 'seller_save_list.dart';
--- lib/app/features/buyer/search/widgets/widgets.dart
+++
@@ -1,4 +0,0 @@
-export 'service_history.dart';
-export 'seller_history.dart';
-export 'search_result.dart';
-export 'filter_dialog.dart';
