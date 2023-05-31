c885ba95d800446a5d27a74af74a17c089fab83c
Le Hai Phong <phong46037@gmail.com>
2023-05-31 09:46:41 +0700

    fix some bug

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,15 +1,11 @@
-
-import 'package:cliver_mobile/app/core/utils/localization_service.dart';
-import 'package:cliver_mobile/app/core/values/app_colors.dart';
-import 'package:cliver_mobile/app/features/setting/setting_controller.dart';
-import 'package:cliver_mobile/app/routes/pages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'controller/user_controller.dart';
-import 'features/authentication/login/login_screen.dart';
-import 'features/bottom_navigation_bar/my_bottom_bar.dart';
-import 'features/onboarding/onboarding.dart';
+import 'core/core.dart';
+import 'core/utils/localization_service.dart';
+import 'features/features.dart';
+import 'routes/pages.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -46,4 +42,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
